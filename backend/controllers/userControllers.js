7635d33eb9b4b01f3e667920ce6ddba0f530e111
Emmanuel Egomson <fegomson@gmail.com>
2022-10-04 16:54:45 -0500

    Register new user

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,12 +1,50 @@
+const User = require('../models/userModel');
+const asyncHandler = require('express-async-handler');
+
 // Register user controller
-const registerUser = (req, res) => {
-  if (!req.body.email) {
-    res.status(404);
-    throw new Error('Please enter an email address');
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  // Validate form fields
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error('Please fill in all required fields');
   }
 
-  res.send('Register user controller');
-};
+  // Validate password
+  if (password.length < 6) {
+    res.status(400);
+    throw new Error('Password must be at least 6 characters long');
+  }
+
+  // Check if email already exists
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('Email already exists');
+  }
+
+  // Create new user
+  const user = await User.create({ name, email, password });
+
+  if (user) {
+    const { _id, name, email, photo, phone, bio } = user;
+
+    res.status(201).json({
+      _id,
+      name,
+      email,
+      password,
+      photo,
+      phone,
+      bio,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
+});
 
 module.exports = {
   registerUser,
