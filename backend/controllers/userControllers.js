f92330fdc2f030f085cae78ab40129edd7abdda0
Emmanuel Egomson <fegomson@gmail.com>
2022-10-05 09:54:49 -0500

    Creates update user controller function

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -207,7 +207,30 @@ const getLogInStatus = asyncHandler(async (req, res) => {
  */
 
 const updateUser = asyncHandler(async (req, res) => {
-  res.send('Updating user information');
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    const { name, email, photo, phone, bio } = user;
+
+    user.email = email;
+    user.name = req.body.name || name;
+    user.photo = req.body.photo || photo;
+    user.phone = req.body.phone || phone;
+    user.bio = req.body.bio || bio;
+
+    const updatedUser = await user.save();
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      photo: updatedUser.photo,
+      phone: updatedUser.phone,
+      bio: updatedUser.bio,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
 });
 
 module.exports = {
