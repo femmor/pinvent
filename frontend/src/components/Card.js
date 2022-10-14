4f862308b54750d2567ab35a8fc6656c753a108f
Emmanuel Egomson <fegomson@gmail.com>
2022-10-13 23:22:24 -0500

    Create card component

---
+++ frontend/src/components/Card.js
@@ -0,0 +1,6 @@
+import styles from '../styles/Card.module.scss';
+
+const Card = ({ children, cardClass }) => {
+  return <div className={`${styles.card} ${cardClass}`}>{children}</div>;
+};
+export default Card;
--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,1 +1,2 @@
 export { default as NumberText } from './NumberText';
+export { default as Card } from './Card';
