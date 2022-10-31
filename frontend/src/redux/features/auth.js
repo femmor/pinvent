f0d0f2541430f89cfdc24c8ea8c3d21f680c3bb3
Emmanuel Egomson <fegomson@gmail.com>
2022-10-31 16:01:52 -0500

    Set up auth slice

--- frontend/src/redux/features/auth.js
+++ frontend/src/redux/features/auth.js
@@ -1,15 +1,46 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const name = JSON.parse(localStorage.getItem('name'));
+
 const initialState = {
   isLoggedIn: false,
+  name: name ? name : '',
+  user: {
+    name: '',
+    email: '',
+    phone: '',
+    bio: '',
+    photo: '',
+  },
 };
 
 const auth = createSlice({
   name: 'auth',
   initialState,
-  reducers: {},
+  reducers: {
+    SET_LOGIN: (state, { payload }) => {
+      state.isLoggedIn = payload;
+    },
+    SET_NAME: (state, { payload }) => {
+      localStorage.setItem('name', JSON.stringify(payload));
+      state.name = payload;
+    },
+    SET_USER: (state, { payload }) => {
+      const profile = payload;
+      state.user.name = profile.user.name;
+      state.user.email = profile.user.email;
+      state.user.phone = profile.user.phone;
+      state.user.bio = profile.user.bio;
+      state.user.photo = profile.user.photo;
+    },
+  },
+  extraReducers: {},
 });
 
-// export const {} = auth.actions;
+export const { SET_LOGIN, SET_NAME, SET_USER } = auth.actions;
+
+export const selectIsLoggedIn = state => state.auth.isLoggedIn;
+export const selectName = state => state.auth.name;
+export const selectUser = state => state.auth.user;
 
 export default auth.reducer;
