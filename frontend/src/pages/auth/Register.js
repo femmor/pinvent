6397b1cad147f3cc52f07a51a9bab2ff34efb995
Emmanuel Egomson <fegomson@gmail.com>
2022-11-01 08:57:18 -0500

    Create  register user state

--- frontend/src/pages/auth/Register.js
+++ frontend/src/pages/auth/Register.js
@@ -1,10 +1,39 @@
+import { useState } from 'react';
 import { TiUserAddOutline } from 'react-icons/ti';
 import { Link } from 'react-router-dom';
 import { Card } from '../../components';
 
 import styles from '../../styles/auth.module.scss';
 
+const initialState = {
+  name: '',
+  email: '',
+  password: '',
+  confirmPassword: '',
+};
+
 const Register = () => {
+  const [formData, setFormData] = useState(initialState);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleInputChange = e => {
+    const { name, value } = e.target;
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const register = e => {
+    e.preventDefault();
+
+    console.log(formData);
+  };
+
+  // Destructure form data
+  const { name, email, password, confirmPassword } = formData;
+
   return (
     <div className={`container ${styles.auth}`}>
       <Card>
@@ -14,25 +43,38 @@ const Register = () => {
           </div>
           <h2>Register</h2>
 
-          <form>
-            <input type="text" name="name" placeholder="Enter name" required />
+          <form onSubmit={register}>
+            <input
+              type="text"
+              name="name"
+              placeholder="Enter name"
+              required
+              value={name}
+              onChange={handleInputChange}
+            />
             <input
               type="email"
               name="email"
               placeholder="Enter email"
               required
+              value={email}
+              onChange={handleInputChange}
             />
             <input
               type="password"
               name="password"
               placeholder="Enter password"
               required
+              value={password}
+              onChange={handleInputChange}
             />
             <input
               type="password"
-              name="password2"
+              name="confirmPassword"
               placeholder="Confirm password"
               required
+              value={confirmPassword}
+              onChange={handleInputChange}
             />
             <button className="--btn --btn-primary --btn-block" type="submit">
               Register
