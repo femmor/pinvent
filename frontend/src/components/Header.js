dd78103961a07e45c1061d880f6955148aa93b8c
Emmanuel Egomson <fegomson@gmail.com>
2022-11-14 10:59:34 -0600

    Display user name

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,11 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { SET_LOGIN } from '../redux/features/auth';
+import { SET_LOGIN, selectName } from '../redux/features/auth';
 import { logoutUser } from '../services/authService';
 
 const Header = () => {
+  const name = useSelector(selectName);
+
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -18,7 +20,7 @@ const Header = () => {
       <div className="--flex-between">
         <h3>
           <span className="--fw-thin">Welcome, </span>
-          <span className="--color-danger">User</span>
+          <span className="--color-danger">{name}</span>
         </h3>
         <button className="--btn --btn-danger" onClick={logout}>
           Logout
