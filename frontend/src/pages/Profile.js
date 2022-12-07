41a823ccd9c19ab138f206adfa2a517e154d38dc
Emmanuel Egomson <fegomson@gmail.com>
2022-12-07 15:39:23 -0600

    Profile component JSX

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -3,7 +3,10 @@ import { useDispatch } from 'react-redux';
 import useRedirectLoggedOut from '../hooks/useRedirectLoggedOut';
 import { SET_NAME, SET_USER } from '../redux/features/auth';
 import { getUser } from '../services/authService';
+import { Loader } from '../components/Loader';
+import { Card } from '../components';
 import '../styles/Profile.scss';
+import { Link } from 'react-router-dom';
 
 const Profile = () => {
   useRedirectLoggedOut('/login');
@@ -29,6 +32,46 @@ const Profile = () => {
     fetchUserData();
   }, [dispatch]);
 
-  return <div className="profile">Profile</div>;
+  return (
+    <div className="profile --my2">
+      {isLoading && (
+        <Loader size={35} borderColor="white" borderTopColor="#007bff" />
+      )}
+      <>
+        {!isLoading && profile === null ? (
+          <>Something went wrong, please reload the page.</>
+        ) : (
+          <Card cardClass={`card --flex-dir-column`}>
+            <span className="profile-photo">
+              <img src={profile?.photo} alt={profile?.name} />
+            </span>
+            <span className="profile-data">
+              <p>
+                <b>Name: </b>
+                {profile?.name}
+              </p>
+              <p>
+                <b>Email: </b>
+                {profile?.email}
+              </p>
+              <p>
+                <b>Phone: </b>
+                {profile?.phone}
+              </p>
+              <p>
+                <b>Bio: </b>
+                {profile?.bio}
+              </p>
+              <div>
+                <Link to="/edit-profile">
+                  <button className="--btn --btn-primary">Edit Profile</button>
+                </Link>
+              </div>
+            </span>
+          </Card>
+        )}
+      </>
+    </div>
+  );
 };
 export default Profile;
